a8ef158fd04b182d317463bf4d36a94f6fec8c07
Sin PRop <Sinproper@gmail.com>
2022-01-16 19:12:52 -0500

    I completed the first jsx project in these files

---
+++ first-react-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+  <div>
+  <h1>Hello Dojo!</h1>
+  <h3>Things i need to do:</h3>
+  <li>Learn React</li>
+  <li>climb Mt. Everest</li>
+  <li>Run a marathon</li>
+  <li>Climb Mt. Everest</li>
+  <li>Feed the Dogs</li>
+
+  </div>);
+}
+
+export default App;
